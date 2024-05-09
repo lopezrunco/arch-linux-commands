01c24bff84f685e2ae9cf70774af92bba01bd4bf
lopezrunco <lopezrunco@gmail.com>
2024-05-09 07:28:49 +0530

    startbrokenlen & backup-vsc-extensions added, startcampo modified: opening in VSC after running servers

--- custom-commands/startcampo.sh
+++ custom-commands/startcampo.sh
@@ -3,3 +3,6 @@ konsole --workdir ~/Damian/campoeventos-com-uy/system/backend --hold -e bash -c 
 
 echo "Starting campoeventos frontend..."
 konsole --workdir ~/Damian/campoeventos-com-uy/system/frontend --hold -e bash -c 'git checkout dev; npm run dev' &
+
+echo "Opening project in VSC..."
+cd ~/Damian/campoeventos-com-uy/system/
